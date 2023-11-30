3268a45b4b258e7818fb094f6a0553583c19cd1e
Henrique <henrique1jose@gmail.com>
2023-11-30 15:13:19 -0300

    atualização

--- app/src/main/java/br/com/nexus/sampledata/SampleData.kt
+++ app/src/main/java/br/com/nexus/sampledata/SampleData.kt
@@ -1,6 +1,5 @@
 package br.com.nexus.sampledata
 
-import br.com.nexus.R
 import br.com.nexus.model.Produto
 import java.math.BigDecimal
 
@@ -8,17 +7,17 @@ val sampleCandies = listOf(
     Produto(
         name = "Chocolate",
         price = BigDecimal("3.99"),
-        image = "https://imagens.pexels.com/photos/65882/chocolate-dark-coffee-confiserie-65882.jpeg",
+        image = "https://images.pexels.com/photos/65882/chocolate-dark-coffee-confiserie-65882.jpeg",
     ),
     Produto(
         name = "Sorvete",
         price = BigDecimal("5.99"),
-        image = "https://imagens.pexels.com/photos/1352278/pexels-photo-1352278.jpeg",
+        image = "https://images.pexels.com/photos/1352278/pexels-photo-1352278.jpeg",
     ),
     Produto(
         name = "Bolo",
         price = BigDecimal("11.99"),
-        image = "https://imagens.pexels.com/photos/291528/pexels-photo-291528.jpeg",
+        image = "https://images.pexels.com/photos/291528/pexels-photo-291528.jpeg",
     )
 )
 
@@ -26,22 +25,22 @@ val sampleDrinks = listOf(
     Produto(
         name = "Cerveja",
         price = BigDecimal("5.99"),
-        image = "https://imagens.pexels.com/photos/1552630/pexels-photo-1552630.jpeg",
+        image = "https://images.pexels.com/photos/1552630/pexels-photo-1552630.jpeg",
     ),
     Produto(
         name = "Refrigerante",
         price = BigDecimal("4.99"),
-        image = "https://imagens.pexels.com/photos/2775860/pexels-photo-2775860.jpeg"
+        image = "https://images.pexels.com/photos/2775860/pexels-photo-2775860.jpeg"
     ),
     Produto(
         name = "Suco",
         price = BigDecimal("7.99"),
-        image = "https://imagens.pexels.com/photos/96974/pexels-photo-96974.jpeg"
+        image = "https://images.pexels.com/photos/96974/pexels-photo-96974.jpeg"
     ),
     Produto(
         name = "Água",
         price = BigDecimal("2.99"),
-        image = "https://imagens.pexels.com/photos/327090/pexels-photo-327090.jpeg"
+        image = "https://images.pexels.com/photos/327090/pexels-photo-327090.jpeg"
     )
 )
 
@@ -49,17 +48,17 @@ val sampleProduto: List<Produto> = listOf(
     Produto(
         name = "Hamburguer",
         price = BigDecimal("12.99"),
-        image = "https://imagens.pexels.com/photos/1639557/pexels-photo-1639557.jpeg"
+        image = "https://images.pexels.com/photos/1639557/pexels-photo-1639557.jpeg"
     ),
     Produto(
         name = "Pizza",
         price = BigDecimal("19.99"),
-        image = "https://imagens.pexels.com/photos/825661/pexels-photo-825661.jpeg"
+        image = "https://images.pexels.com/photos/825661/pexels-photo-825661.jpeg"
     ),
     Produto(
         name = "Batata Frita",
         price = BigDecimal("7.99"),
-        image = "https://imagens.pexels.com/photos/1583884/pexels-photo-1583884.jpeg"
+        image = "https://images.pexels.com/photos/1583884/pexels-photo-1583884.jpeg"
     ), *sampleDrinks.toTypedArray(), *sampleCandies.toTypedArray()
 )
 
--- app/src/main/java/br/com/nexus/ui/theme/screens/InicialScreen.kt
+++ app/src/main/java/br/com/nexus/ui/theme/screens/InicialScreen.kt
@@ -1,15 +1,34 @@
 package br.com.nexus.ui.theme.screens
 
+import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.nexus.R
@@ -17,34 +36,63 @@ import br.com.nexus.model.Produto
 import br.com.nexus.sampledata.sampleSections
 import br.com.nexus.ui.theme.components.SelecaoProduto
 import java.math.BigDecimal
+import kotlin.random.Random
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun InicialScreen(
     sections: Map<String, List<Produto>>
 ) {
-    LazyColumn(
-        Modifier
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-        contentPadding = PaddingValues(vertical = 16.dp)
-    ) {
-        item {
-            Spacer(Modifier)
-        }
-
-        for (section in sections){
-            val title = section.key
-            val produto = section.value
+    Column {
+        var text by remember { mutableStateOf("") }
+        OutlinedTextField(
+            value = text,
+            onValueChange = { newValue ->
+                text = newValue
+            },
+            Modifier
+                .padding(
+                    start = 16.dp,
+                    top = 16.dp,
+                    end = 16.dp
+                )
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(75),
+            leadingIcon = {
+                Icon(Icons.Default.Search, contentDescription = "Lupa")
+            },
+            label = {
+                Text(text = "Produto")
+            },
+            placeholder = {
+                Text(text = "O que você deseja?")
+            })
+        LazyColumn(
+            Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            contentPadding = PaddingValues(vertical = 16.dp)
+        ) {
             item {
-                SelecaoProduto(
-                    title = title,
-                    produtos = produto)
+                Spacer(Modifier)
             }
-        }
-        item {
-            Spacer(Modifier)
-        }
 
+            for (section in sections) {
+                val title = section.key
+                val produto = section.value
+                item {
+                    SelecaoProduto(
+                        title = title,
+                        produtos = produto
+                    )
+                }
+            }
+            item {
+                Spacer(Modifier)
+            }
+
+        }
     }
 }
 
